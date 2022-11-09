d14b5872dd7f08ac8bc094bbc4c1171ff118226c
Yamaki09 <kojie09yamashita@gmail.com>
2022-11-09 22:38:34 +0900

    feat: updates on components

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Homepage() {
-	return <h1>Hello Homepage</h1>;
+	return (
+		<>
+			<h1 className="title-home">Welcome to Nin-Money</h1>
+			<img src="https://i.ibb.co/9nsTqfG/budget.png" className="icon" />
+			<h4 className="desc">
+				Where you can keep track of your money and expenses.
+			</h4>
+		</>
+	);
 }
+
+//<a href="https://imgbb.com/"><img src="https://i.ibb.co/9nsTqfG/budget.png" alt="budget" border="0"></a>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,51 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 export default function Signup() {
-	return <h1>Hello Signup</h1>;
+	return (
+		<>
+			<div className="form">
+				<div className="signup-fname">
+					<label className="signup-form" for="first_name">
+						First Name
+					</label>
+					<input
+						className="signup-input"
+						type="text"
+						placeholder="First Name"
+					/>
+					<br />
+					<label className="signup-form" for="last_name">
+						Last Name
+					</label>
+					<input className="signup-input" type="text" placeholder="Last Name" />
+					<br />
+					<label className="signup-form" for="email">
+						Email
+					</label>
+					<input className="signup-input" type="text" placeholder="Email" />
+					<br />
+					<label className="signup-form" for="password">
+						Password
+					</label>
+					<input className="signup-input" type="text" placeholder="Password" />
+					<br />
+					<label className="signup-form" for="password2">
+						Confirm Password
+					</label>
+					<input
+						className="signup-input"
+						type="text"
+						placeholder="Confirm Password"
+					/>
+				</div>
+			</div>
+			<Box textAlign="center">
+				<Button className="signup-button" variant="contained">
+					Signup
+				</Button>
+			</Box>
+		</>
+	);
 }
