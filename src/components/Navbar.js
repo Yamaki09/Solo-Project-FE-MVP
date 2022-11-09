c2fbe1216e26095f724e3d81f73b7b7a6ae7e1d3
Yamaki09 <kojie09yamashita@gmail.com>
2022-11-09 20:40:07 +0900

    feat: created components

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+	return (
+		<>
+			<div className="navbar">
+				<h1 className="title">Nin-Money</h1>
+				<Breadcrumbs className="nav-option" aria-label="breadcrumb">
+					<Link to="/main" underline="hover" color="inherit">
+						Home
+					</Link>
+					<Link to="/login" underline="hover" color="inherit">
+						Login
+					</Link>
+					<Link to="/signup" underline="hover" color="inherit">
+						Signup
+					</Link>
+				</Breadcrumbs>
+			</div>
+		</>
+	);
+}
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PageNotFound() {
+	return (
+		<>
+			<h1>Page not found</h1>
+			<h3>Error: 404</h3>
+		</>
+	);
+}
